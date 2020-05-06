e28ae1094f1345390f8d519475ba457ff22757a3
Vincent Chi <song374561@chivincent.net>
2020-05-06 11:27:07 +0800

    feat: handle user disconnect event.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-io.on('connection', socket => console.log('user connected'));
+io.on('connection', socket => {
+    console.log('user connected')
+
+    socket.on('disconnect', () => console.log('user disconnected'));
+});
 
 http.listen(3000, () => console.log('listen on *:3000'));
