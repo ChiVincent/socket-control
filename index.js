7b6debf98a77c183fb7d6c547d818d35ca79c283
Vincent Chi <song374561@chivincent.net>
2020-05-06 11:21:24 +0800

    feat: handle socket.io connection in server.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let app = require('express')();
 let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+io.onconnection(socket => console.log('user connected'));
 
 http.listen(3000, () => console.log('listen on *:3000'));
