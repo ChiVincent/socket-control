27c3f63ef14bcb59accd69af7f345889e79235a3
Vincent Chi <song374561@chivincent.net>
2020-05-06 11:47:48 +0800

    feat: broadcast the power and stage change.

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ let io = require('socket.io')(http);
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 io.on('connection', socket => {
-    socket.on('power', msg => console.log(`Power: ${msg}`));
-    socket.on('stage', stage => console.log(`Stage: ${stage}`));
+    socket.on('power', msg => {
+        console.log(`Power: ${msg}`);
+        io.emit('power', msg);
+    });
+    socket.on('stage', stage => {
+        console.log(`Stage: ${stage}`);
+        io.emit('stage', stage);
+    });
 });
 
 http.listen(3000, () => console.log('listen on *:3000'));
