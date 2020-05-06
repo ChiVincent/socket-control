77427c5e46dcbff9666cf548030a283b92771a60
Vincent Chi <song374561@chivincent.net>
2020-05-06 11:23:49 +0800

    feat: connect socket.io in front-end.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-io.onconnection(socket => console.log('user connected'));
+io.on('connection', socket => console.log('user connected'));
 
 http.listen(3000, () => console.log('listen on *:3000'));
