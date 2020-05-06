8e2f5048a4cc224c80abe98f22d18987c3643087
Vincent Chi <song374561@chivincent.net>
2020-05-06 11:05:54 +0800

    feat: serve a static html file.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let app = require('express')();
 let http = require('http').createServer(app);
 
-app.get('/', (req, res) => res.send('<h1>Hello World</h1>'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 http.listen(3000, () => console.log('listen on *:3000'));
